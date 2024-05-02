eb447c66888db6e3502e81ae9c034825bffd2f4e
ARamos <albertramos902@gmail.com>
2024-05-02 18:56:37 +0100

    using tosorted new method to sort by country

--- src/App.tsx
+++ src/App.tsx
@@ -6,10 +6,20 @@ import UsersList from './components/UsersList'
 function App() {
   const [users, setUsers] = useState<User[]>([])
   const [showColors, setShowColors] = useState(false)
+  const [sortByCountry, setSortByCountry] = useState(false)
 
   const toggleColors = () => {
     setShowColors(!showColors);
   }
+
+  const ToggleSortByCountry = () => {
+    setSortByCountry(prevState => !prevState)
+  }
+
+  const sortedUsers = sortByCountry ? users.toSorted((a, b) => {
+    return a.location.country.localeCompare(b.location.country);
+}) : users
+
   
   useEffect(() => {
     fetch("https://randomuser.me/api/?results=100")
@@ -28,8 +38,9 @@ function App() {
       <h1>List of users</h1>
       <header>
         <button onClick={toggleColors}>Colors</button>
+        <button onClick={ToggleSortByCountry}>{sortByCountry ? 'Remove sort' : 'Sorty by country'} </button>
       </header>
-      <UsersList users={users} showColors={showColors} />
+      <UsersList users={sortedUsers} showColors={showColors} />
     </>
   )
 }
