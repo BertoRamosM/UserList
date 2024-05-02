9dee3cbaa03d25760b32a97c540d336e09d4d246
ARamos <albertramos902@gmail.com>
2024-05-02 19:24:36 +0100

    useref to store original array and button to restore it

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { type User } from './types'
-import { useEffect, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import './App.css'
 import UsersList from './components/UsersList'
 
@@ -7,6 +7,7 @@ function App() {
   const [users, setUsers] = useState<User[]>([])
   const [showColors, setShowColors] = useState(false)
   const [sortByCountry, setSortByCountry] = useState(false)
+  const originalUsers = useRef<User[]>([])
 
   const toggleColors = () => {
     setShowColors(!showColors);
@@ -16,13 +17,18 @@ function App() {
     setSortByCountry(prevState => !prevState)
   }
 
+  const handleReset = () => {
+    setUsers(originalUsers.current)
+  }
+
   const sortedUsers = sortByCountry ? users.toSorted((a, b) => {
     return a.location.country.localeCompare(b.location.country);
   }) : users
   
   const handleDelete = (email: string) => {
     const filteredUsers = users.filter((user) => user.email !== email)
-      setUsers(filteredUsers) 
+    setUsers(filteredUsers) 
+   
   }
 
   
@@ -31,6 +37,7 @@ function App() {
       .then((res) => res.json())
       .then((data) => {
         setUsers(data.results)
+         originalUsers.current = data.results;
       })
       .catch(error => {
         console.error(error)
@@ -44,6 +51,7 @@ function App() {
       <header>
         <button onClick={toggleColors}>Colors</button>
         <button onClick={ToggleSortByCountry}>{sortByCountry ? 'Remove sort' : 'Sorty by country'} </button>
+        <button onClick={handleReset}>Restore original users</button>
       </header>
       <UsersList users={sortedUsers} showColors={showColors} handleDelete={handleDelete} />
     </>
